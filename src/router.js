24dc8332557f95574a14138b12fb2810c548c3eb
Radu Milea <radu.milea.d@gmail.com>
2019-08-08 16:36:15 +0300

    save store data

--- src/router.js
+++ src/router.js
@@ -4,6 +4,7 @@ import Home from './views/Home.vue';
 import About from './views/About.vue';
 import Victory from './views/Victory.vue';
 import Failed from './views/Failed.vue';
+import store from './store';
 
 Vue.use(Router);
 
@@ -15,6 +16,22 @@ export default new Router({
       path: '/',
       name: 'home',
       component: Home,
+      beforeEnter: (to, from, next) => {
+        // IF Hero HP <= 0 redirect to failed page
+        const heroHP = store.state.hero.HP;
+        if (heroHP <= 0) {
+          next('/failed');
+        }
+
+        // IF Enemy HP <= 0 redirect to victory page
+        const enemyHP = store.state.enemy.HP;
+        if (enemyHP <= 0) {
+          next('/victory');
+        }
+
+        // Go to Home page
+        next();
+      },
     },
     {
       path: '/about',
--- src/store.js
+++ src/store.js
@@ -1,10 +1,17 @@
 import Vue from 'vue';
 import Vuex from 'vuex';
 import router from './router';
+import VuexPersist from 'vuex-persist';
 
 Vue.use(Vuex);
 
+const vuexPersist = new VuexPersist({
+  key: 'my-app',
+  storage: localStorage,
+});
+
 export default new Vuex.Store({
+  plugins: [vuexPersist.plugin],
   state: {
     hero: {
       maxHP: 1200,
